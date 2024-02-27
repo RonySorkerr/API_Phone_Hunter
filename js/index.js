42e1f55c99e10ba1542e598f2586c4d0c8375df8
RonySorkerr <mdronysarkar33299@gmail.com>
2024-02-27 20:49:03 +0600

    phones grid added

--- js/index.js
+++ js/index.js
@@ -2,13 +2,40 @@
 // .then(res => res.json())
 // .then(data => console.log(data))
 
-const loadPhones = async () =>{
+const loadPhones = async () => {
     const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
     const data = await res.json();
     const phones = data.data;
-    console.log(phones)
+    displayPhones(phones)
+    // console.log(phones)
     // console.log(data.data[0].phone_name);
 
+}
+
+
+const displayPhones = phones => {
+    const phoneContainer = document.getElementById('phnsCont');
+    phones.forEach(phone => {
+        // console.log(phone)
+        /**
+         * 01. 
+         */
+        const phoneCard = document.createElement('div');
+        phoneCard.classList = `card bg-teal-400 shadow-xl`;
+        phoneCard.innerHTML = `
+            <figure class="px-10 pt-10">
+                <img src="${phone.image}" alt="Shoes" class="rounded-xl" />
+                </figure>
+            <div class="card-body items-center text-center">
+                <h2 class="card-title">${phone.phone_name}</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions">
+                <button class="btn btn-primary">Buy Now</button>
+            </div>
+            </div>
+        `;
+        phoneContainer.appendChild(phoneCard);
+    })
 }
 
 loadPhones();
