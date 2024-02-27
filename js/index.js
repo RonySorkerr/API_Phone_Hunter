3b279e648ad7be882cb1751d88b3fa9db1f95eab
RonySorkerr <mdronysarkar33299@gmail.com>
2024-02-27 20:31:11 +0600

    card adding

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
-alert()
+// fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+// .then(res => res.json())
+// .then(data => console.log(data))
+
+const loadPhones = async () =>{
+    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+    const data = await res.json();
+    const phones = data.data;
+    console.log(phones)
+    // console.log(data.data[0].phone_name);
+
+}
+
+loadPhones();
